0bc31dcef77ae28da4171db2df3b692c2acf6039
hterrence <harristerrence78@gmail.com>
2018-01-03 20:15:13 +0100

    Updated data

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,87 +1,87 @@
 const tableData = {
     "period" : {
-        "from" : "2018-01-01T20:15:57.753Z",
-        "to" : "2018-01-02T19:18:45.514Z",
-        "prettyFrom" : "2018-01-01 20:15 UTC",
-        "prettyTo" : "2018-01-02 19:18 UTC"
+        "from" : "2018-01-02T19:18:45.514Z",
+        "to" : "2018-01-03T19:13:18.989Z",
+        "prettyFrom" : "2018-01-02 19:18 UTC",
+        "prettyTo" : "2018-01-03 19:13 UTC"
     },
     "infos" : [
         {
             "name" : "ICO Check",
             "id" : "checkico",
-            "msgSinceLastUpdate" : 267,
-            "usersCount" : 382,
-            "newUsersSinceLastUpdate" : 12
+            "msgSinceLastUpdate" : 126,
+            "usersCount" : 409,
+            "newUsersSinceLastUpdate" : 27
         },
         {
             "name" : "Nucleus Vision",
             "id" : "nucleusvision",
-            "msgSinceLastUpdate" : 4602,
-            "usersCount" : 23255,
-            "newUsersSinceLastUpdate" : 1115
+            "msgSinceLastUpdate" : 4449,
+            "usersCount" : 24229,
+            "newUsersSinceLastUpdate" : 974
         },
         {
             "name" : "Jibrel Network",
             "id" : "jibrel_network",
-            "msgSinceLastUpdate" : 443,
-            "usersCount" : 2934,
-            "newUsersSinceLastUpdate" : 36
+            "msgSinceLastUpdate" : 387,
+            "usersCount" : 2954,
+            "newUsersSinceLastUpdate" : 20
         },
         {
             "name" : "indaHash",
             "id" : "indahash_chat",
-            "msgSinceLastUpdate" : 728,
-            "usersCount" : 11536,
+            "msgSinceLastUpdate" : 926,
+            "usersCount" : 11567,
             "newUsersSinceLastUpdate" : 31
         },
         {
             "name" : "Quantstamp",
             "id" : "quantstamp",
-            "msgSinceLastUpdate" : 5512,
-            "usersCount" : 19305,
-            "newUsersSinceLastUpdate" : 238
+            "msgSinceLastUpdate" : 3337,
+            "usersCount" : 19475,
+            "newUsersSinceLastUpdate" : 170
         },
         {
             "name" : "SALPay",
             "id" : "salpayico",
-            "msgSinceLastUpdate" : 721,
-            "usersCount" : 2341,
-            "newUsersSinceLastUpdate" : 23
+            "msgSinceLastUpdate" : 418,
+            "usersCount" : 2369,
+            "newUsersSinceLastUpdate" : 28
         },
         {
             "name" : "Bounty0x",
             "id" : "bounty0x",
-            "msgSinceLastUpdate" : 1250,
-            "usersCount" : 3525,
-            "newUsersSinceLastUpdate" : 56
+            "msgSinceLastUpdate" : 2181,
+            "usersCount" : 3562,
+            "newUsersSinceLastUpdate" : 37
         },
         {
             "name" : "INS",
             "id" : "ins_ecosystem",
-            "msgSinceLastUpdate" : 1485,
-            "usersCount" : 18302,
-            "newUsersSinceLastUpdate" : 22
+            "msgSinceLastUpdate" : 1782,
+            "usersCount" : 18332,
+            "newUsersSinceLastUpdate" : 30
         },
         {
             "name" : "Bread",
             "id" : "breadchat",
-            "msgSinceLastUpdate" : 2443,
-            "usersCount" : 8187,
-            "newUsersSinceLastUpdate" : 2
+            "msgSinceLastUpdate" : 1730,
+            "usersCount" : 8227,
+            "newUsersSinceLastUpdate" : 40
         },
         {
             "name" : "Ripple",
             "id" : "ripple",
-            "msgSinceLastUpdate" : 8190,
-            "usersCount" : 32071,
-            "newUsersSinceLastUpdate" : 883
+            "msgSinceLastUpdate" : 9002,
+            "usersCount" : 32948,
+            "newUsersSinceLastUpdate" : 877
         },
         {
             "name" : "Crypterium",
             "id" : "crypterium",
-            "msgSinceLastUpdate" : 10792,
-            "usersCount" : 25411,
-            "newUsersSinceLastUpdate" : 1165
+            "msgSinceLastUpdate" : 10534,
+            "usersCount" : 26961,
+            "newUsersSinceLastUpdate" : 1550
         }
     ]
 };
