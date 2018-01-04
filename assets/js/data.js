338b556b301c12f4f436ac8f044c3f2eff99f765
hterrence <harristerrence78@gmail.com>
2018-01-04 20:25:36 +0100

    Updated data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,87 +1,122 @@
 const tableData = {
     "period" : {
-        "from" : "2018-01-02T19:18:45.514Z",
-        "to" : "2018-01-03T19:13:18.989Z",
-        "prettyFrom" : "2018-01-02 19:18 UTC",
-        "prettyTo" : "2018-01-03 19:13 UTC"
+        "from" : "2018-01-03T19:13:18.989Z",
+        "to" : "2018-01-04T19:21:51.523Z",
+        "prettyFrom" : "2018-01-03 19:13 UTC",
+        "prettyTo" : "2018-01-04 19:21 UTC"
     },
     "infos" : [
         {
-            "name" : "ICO-Check",
+            "name" : "ICO Check",
             "id" : "checkico",
-            "msgSinceLastUpdate" : 126,
-            "usersCount" : 409,
-            "newUsersSinceLastUpdate" : 27
+            "msgSinceLastUpdate" : 161,
+            "usersCount" : 424,
+            "newUsersSinceLastUpdate" : 15
         },
         {
             "name" : "Nucleus Vision",
             "id" : "nucleusvision",
-            "msgSinceLastUpdate" : 4449,
-            "usersCount" : 24229,
-            "newUsersSinceLastUpdate" : 974
+            "msgSinceLastUpdate" : 4309,
+            "usersCount" : 25238,
+            "newUsersSinceLastUpdate" : 1009
         },
         {
             "name" : "Jibrel Network",
             "id" : "jibrel_network",
-            "msgSinceLastUpdate" : 387,
-            "usersCount" : 2954,
-            "newUsersSinceLastUpdate" : 20
+            "msgSinceLastUpdate" : 327,
+            "usersCount" : 3002,
+            "newUsersSinceLastUpdate" : 48
         },
         {
             "name" : "indaHash",
             "id" : "indahash_chat",
-            "msgSinceLastUpdate" : 926,
-            "usersCount" : 11567,
-            "newUsersSinceLastUpdate" : 31
+            "msgSinceLastUpdate" : 1028,
+            "usersCount" : 11631,
+            "newUsersSinceLastUpdate" : 64
         },
         {
             "name" : "Quantstamp",
             "id" : "quantstamp",
-            "msgSinceLastUpdate" : 3337,
-            "usersCount" : 19475,
-            "newUsersSinceLastUpdate" : 170
+            "msgSinceLastUpdate" : 4560,
+            "usersCount" : 19679,
+            "newUsersSinceLastUpdate" : 204
         },
         {
             "name" : "SALPay",
             "id" : "salpayico",
-            "msgSinceLastUpdate" : 418,
-            "usersCount" : 2369,
-            "newUsersSinceLastUpdate" : 28
+            "msgSinceLastUpdate" : 459,
+            "usersCount" : 2392,
+            "newUsersSinceLastUpdate" : 23
         },
         {
             "name" : "Bounty0x",
             "id" : "bounty0x",
-            "msgSinceLastUpdate" : 2181,
-            "usersCount" : 3562,
-            "newUsersSinceLastUpdate" : 37
+            "msgSinceLastUpdate" : 1022,
+            "usersCount" : 3643,
+            "newUsersSinceLastUpdate" : 81
         },
         {
             "name" : "INS",
             "id" : "ins_ecosystem",
-            "msgSinceLastUpdate" : 1782,
-            "usersCount" : 18332,
-            "newUsersSinceLastUpdate" : 30
+            "msgSinceLastUpdate" : 1666,
+            "usersCount" : 18347,
+            "newUsersSinceLastUpdate" : 15
         },
         {
             "name" : "Bread",
             "id" : "breadchat",
-            "msgSinceLastUpdate" : 1730,
-            "usersCount" : 8227,
-            "newUsersSinceLastUpdate" : 40
+            "msgSinceLastUpdate" : 2498,
+            "usersCount" : 8302,
+            "newUsersSinceLastUpdate" : 75
         },
         {
             "name" : "Ripple",
             "id" : "ripple",
-            "msgSinceLastUpdate" : 9002,
-            "usersCount" : 32948,
-            "newUsersSinceLastUpdate" : 877
+            "msgSinceLastUpdate" : 15224,
+            "usersCount" : 34832,
+            "newUsersSinceLastUpdate" : 1884
         },
         {
             "name" : "Crypterium",
             "id" : "crypterium",
-            "msgSinceLastUpdate" : 10534,
-            "usersCount" : 26961,
-            "newUsersSinceLastUpdate" : 1550
+            "msgSinceLastUpdate" : 13064,
+            "usersCount" : 28839,
+            "newUsersSinceLastUpdate" : 1878
+        },
+        {
+            "name" : "trade.io",
+            "id" : "TradeToken",
+            "msgSinceLastUpdate" : 0,
+            "usersCount" : 10146,
+            "newUsersSinceLastUpdate" : null
+        },
+        {
+            "name" : "Bluzelle",
+            "id" : "bluzelle",
+            "msgSinceLastUpdate" : 24,
+            "usersCount" : 16583,
+            "newUsersSinceLastUpdate" : null
+        },
+        {
+            "name" : "CoinLion",
+            "id" : "coinlion_platform",
+            "msgSinceLastUpdate" : 3,
+            "usersCount" : 1662,
+            "newUsersSinceLastUpdate" : null
+        },
+        {
+            "name" : "HOQU",
+            "id" : "hoquio",
+            "msgSinceLastUpdate" : 4,
+            "usersCount" : 7178,
+            "newUsersSinceLastUpdate" : null
+        },
+        {
+            "name" : "Envion",
+            "id" : "envion",
+            "msgSinceLastUpdate" : 13,
+            "usersCount" : 16090,
+            "newUsersSinceLastUpdate" : null
         }
     ]
 };
